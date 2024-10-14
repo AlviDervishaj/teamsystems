9b7642b05fc97c7f84249f3f067b74920f6d440a
AlviDervishaj <alvidervishaj9@gmail.com>
2024-10-14 20:19:12 +0200

    Removed Week_1 as it was just an intro to react. Added Week_2 submodule.

--- task_1/react/src/components/Main.jsx
+++
@@ -1,5 +0,0 @@
-export const Main = () => {
-  return (
-    <h2>ABC</h2>
-  )
-};
--- task_1/react/src/components/ToggleTabs/index.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./index.module.css";
-export const ToggleTabs = ({ activeTab, setActiveTab }) => {
-  return (
-    <section className={styles.button_group}>
-      <button className={`${styles.button} ${activeTab === "main" ? styles.active : ""}`} onClick={() => setActiveTab("main")}>Main</button>
-      <button className={`${styles.button} ${activeTab === "events" ? styles.active : ""}`} onClick={() => setActiveTab("events")}>Events</button>
-    </section>
-  )
-}
